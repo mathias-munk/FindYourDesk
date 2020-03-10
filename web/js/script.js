bc2a2b24aa35241a940e834bd29bae6ee45c2637
George Webberley <georgewebberley@googlemail.com>
2020-03-10 17:16:08 +0000

    Server communicating with website

---
+++ web/js/script.js
@@ -0,0 +1,67 @@
+// Create a client instance, we create a random id so the broker will allow multiple sessions
+
+client = new Paho.MQTT.Client(
+  "broker.mqttdashboard.com",
+  8000,
+  "clientId" + makeid(3)
+);
+
+// client = new Paho.MQTT.Client(
+//   "broker.mqttdashboard.com",
+//   8000,
+//   "clientId123
+// );
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({ onSuccess: onConnect });
+
+function makeBooking() {
+  console.log("makeBooking");
+  client.subscribe("chair_booking");
+  message = new Paho.MQTT.Message("George");
+  message.destinationName = "chair_booking";
+  client.send(message);
+}
+
+function updateTable(payload) {
+  var tr;
+  tr = $("<tr/>");
+  tr.append("<td>" + json[0].order_id + "</td>");
+  tr.append("<td>" + json[1].status + "</td>");
+  tr.append("<td>" + json[4].delivery_address + "</td>");
+  $("table").append(tr);
+}
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made report.
+  console.log("Connected");
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:" + responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:" + message.payloadString);
+}
+
+// called to generate the IDs
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
