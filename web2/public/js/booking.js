595616221ec0b5169f26bf7905835357fb34c17b
George Webberley <georgewebberley@googlemail.com>
2020-04-27 13:54:04 +0100

    Adjusted rooms so that number of chairs updates dynamically

--- web2/public/js/booking.js
+++ web2/public/js/booking.js
@@ -1,3 +1,9 @@
-function makeBooking(id) {
-  $("#result").load("/room");
-}
+const chairDOM = document.querySelectorAll(".chair");
+
+document.addEventListener("DOMContentLoaded", () => {
+  chairDOM.forEach(chair => {
+    if (!chair.classList.contains("free")) {
+      chair.classList.remove("book_hover");
+    }
+  });
+});
